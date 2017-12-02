14ca1e897527672b6cb6a9270ab01f6d28d922c7
ACsiD <drake@popalot.net>
2017-12-01 21:21:42 -0500

    map loaded

--- main.lua
+++ main.lua
@@ -1,3 +1,27 @@
-function love.draw()
+local sti = require "libs.sti"
+local bump = require "libs.bump"
+
+
+function love.load()
+	windowWidth = love.graphics.getWidth()
+	windowHeight = love.graphics.getHeight()
 	
+	love.physics.setMeter(16)
+	map = sti("map/dev.lua",{"bump"})
+	
+	world = bump.newWorld()
+	map:bump_init(world)
+	
+	
+	
+	
+end
+
+function love.draw()
+	map:draw()
+	love.graphics.print("LD40 - The more you have the worst it is",1,1)
+end
+
+function love.update(dt)
+	map:update(dt)
 end
---
+++ map/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="240" columns="20">
+ <image source="../images/tileset.png" trans="ff00ff" width="640" height="400"/>
+</tileset>
